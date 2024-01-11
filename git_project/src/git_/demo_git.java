c01204aaa3f78d93cb4d2cc77ed4d03e31ea8470
Raju <Raju@LAPTOP-Q4CHNOA3>
2024-01-11 11:40:31 +0530

    by kalayni

--- git_project/src/git_/demo_git.java
+++ git_project/src/git_/demo_git.java
@@ -2,8 +2,36 @@ package git_;
 
 public class demo_git {
 
-	public static void main(String[] args) {
+
+	public void  m1()
+	{
+		System.out.println("this is m1");
+	}
+	
+	public void  m2()
+	{
+		System.out.println("this is m2");
+	}
+	
+	public void  m3()
+	{
+		System.out.println("this is m3");
+	}
+	
+	public void  m4()
+	{
+		System.out.println("this is m4");
+	}
+	
+	public void  m5()
+	{
+		System.out.println("this is m5");
+	}
+	
+	public static void main(String[] args) 
+	{
 		System.out.println("hello git");
+		System.out.println("git");
 
 	}
 
